9cb89d12e878c12dbccd85b831e4c2f19f3a7b82
Denis Zahariev <denis.zaharievv@gmail.com>
2021-03-14 03:38:04 +0200

    Added control over motors from the AI

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import socket
 import time
 import imutils
 import cv2
+from motorControll import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True,
@@ -20,12 +21,52 @@ rpiName = socket.gethostname()
 vs = VideoStream(src=-1).start()
 time.sleep(2.0)
 
-while True:
+frame_width = 640
+frame_height = 480
 
+def get_angle(power):
+    normalized = power / (frame_width//2)
+    cam_fov = 70
+    angle = (cam_fov//2) * normalized
+    return angle
+
+rotate_speed = 8
+def take_action(command, power):
+        global moveable
+        if command == "LEFT":
+                angle = get_angle(power)
+                left()
+                time.sleep((rotate_speed*angle)/90)
+                stop()
+                
+        if command == "RIGHT":
+                angle = get_angle(power)
+                right()
+                time.sleep((rotate_speed*angle)/90)
+                stop()
+
+        if command == "FORWARD":
+                angle = get_angle(power)
+                forward()
+                time.sleep((power/40))
+                stop()
+
+        if command == "PICKUP":
+                #TODO
+                pass
+        moveable = True
+
+
+moveable = False
+while True:
+        global moveable
         frame = vs.read()
-        frame=imutils.resize(frame,width=640,height=200)
+        frame=imutils.resize(frame,width=frame_width,height=frame_height)
         answer = sender.send_image(rpiName, frame).decode("ascii")
-        print(answer)
+        command, power = answer.split[" "]
+        if moveable:
+                take_action(command, power)
+
         if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 vs.stream.strea.release()
---
+++ motorControll.py
@@ -0,0 +1,124 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23
+ECHO = 24
+TRIG_2 = 22
+ECHO_2 = 27
+
+R_MOTOR_1 = 5 
+R_MOTOR_2 = 6 
+
+L_MOTOR_1 = 25 
+L_MOTOR_2 = 17 
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(TRIG_2, GPIO.OUT)
+GPIO.setup(ECHO_2, GPIO.IN)
+GPIO.setup(R_MOTOR_1, GPIO.OUT)
+GPIO.setup(R_MOTOR_2, GPIO.OUT)
+GPIO.setup(L_MOTOR_1, GPIO.OUT)
+GPIO.setup(L_MOTOR_2, GPIO.OUT)
+
+isMoved = False
+
+def backwards():
+        global isMoved
+        if isMoved == False:
+                isMoved = True
+                GPIO.output(R_MOTOR_1, GPIO.HIGH)
+                GPIO.output(L_MOTOR_1, GPIO.LOW)
+                GPIO.output(R_MOTOR_2, GPIO.LOW)
+                GPIO.output(L_MOTOR_2, GPIO.HIGH)
+                isMoved = False
+
+def forward():
+        global isMoved
+        if isMoved == False:
+                isMoved = True
+                GPIO.output(R_MOTOR_2, GPIO.HIGH)
+                GPIO.output(L_MOTOR_2, GPIO.LOW)
+                GPIO.output(R_MOTOR_1, GPIO.LOW)
+                GPIO.output(L_MOTOR_1, GPIO.HIGH)
+                isMoved = False
+
+def left():
+        global isMoved
+        if isMoved == False:
+                isMoved = True
+                GPIO.output(R_MOTOR_1, GPIO.LOW)
+                GPIO.output(L_MOTOR_2, GPIO.HIGH)
+                GPIO.output(R_MOTOR_2, GPIO.HIGH)
+                GPIO.output(L_MOTOR_1, GPIO.LOW)
+                isMoved = False
+
+def right():
+        global isMoved
+        if isMoved == False:
+                isMoved = True
+                GPIO.output(R_MOTOR_2, GPIO.HIGH)
+                GPIO.output(L_MOTOR_1, GPIO.LOW)
+                GPIO.output(R_MOTOR_1, GPIO.LOW)
+                GPIO.output(L_MOTOR_2, GPIO.HIGH)
+                isMoved = False
+
+def stop():
+        global isMoved
+        if isMoved == False:
+                isMoved = True
+                GPIO.output(R_MOTOR_1, GPIO.LOW)
+                GPIO.output(L_MOTOR_1, GPIO.LOW)
+                GPIO.output(R_MOTOR_2, GPIO.LOW)
+                GPIO.output(L_MOTOR_2, GPIO.LOW)
+                isMoved = False
+
+if __name__ == "__main__":  
+        try:
+                while True:
+                        GPIO.output(TRIG, False)
+                        print("Waiting for sensor to settle")
+                        time.sleep(2)
+
+                        GPIO.output(TRIG, True)
+                        time.sleep(0.1)
+                        GPIO.output(TRIG, False)
+
+                        while GPIO.input(ECHO)==0:
+                                pulse_start = time.time()
+
+                        while GPIO.input(ECHO)==1:
+                                pulse_end = time.time()
+
+                        pulse_duration = pulse_end - pulse_start
+                        distance = pulse_duration * 17150
+                        distance = round(distance, 2)
+                        print("Distance: "+ distance+ "cm")
+
+                        GPIO.output(TRIG_2, False)
+                        print("Waiting for sensor2 to settle")
+                        time.sleep(2)
+
+                        GPIO.output(TRIG_2, True)
+                        time.sleep(0.00001)
+                        GPIO.output(TRIG_2, False)
+
+                        while GPIO.input(ECHO_2)==0:
+                                pulse_start_2 = time.time()
+
+                        while GPIO.input(ECHO_2)==1:
+                                pulse_end_2 = time.time()
+
+                        pulse_duration_2 = pulse_end_2 - pulse_start_2
+                        distance_2 = pulse_duration_2 * 17150
+                        distance_2 = round(distance_2, 2)
+                        print("Distance: " + distance_2 + "cm")
+
+                        if distance <= 41 or distance_2 <= 41:
+                                stop()
+
+        except KeyboardInterrupt:
+                print("Cleaning up!")
+                GPIO.cleanup()
+
